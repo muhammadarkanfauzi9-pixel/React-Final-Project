9560b9eec45901ee858cc57ebc025da8f75b7574
resha <faldanyudis0@gmail.com>
2025-10-05 18:11:12 +0700

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { favoriteReducer } from "../reducer/favoriteReducer"; // 🧠 named import
+import { configureStore } from "@reduxjs/toolkit"; // Import configureStore dari redux toolkit
+import { favoriteReducer } from "../reducer/favoriteReducer"; // Import reducer favorite
 
+// Konfigurasi store utama Redux
 const Store = configureStore({
   reducer: {
-    favorite: favoriteReducer,
+    favorite: favoriteReducer, // Tambahkan reducer favorite ke dalam store
   },
 });
 
-export default Store;
+export default Store; // Export store
