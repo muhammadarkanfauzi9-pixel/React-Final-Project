c5e4a1c8482f632d0190e73bb01ec0a5a6ba4928
resha <faldanyudis0@gmail.com>
2025-10-05 18:23:26 +0700

    Update FavoriteComponent.jsx

--- src/components/Favorite/FavoriteComponent.jsx
+++ src/components/Favorite/FavoriteComponent.jsx
@@ -1,14 +1,27 @@
-// src/components/Favorite/Favorite.jsx
+// Import React untuk membuat komponen
 import React from "react";
+
+// Import useSelector dari react-redux untuk mengambil data dari Redux store
 import { useSelector } from "react-redux";
+
+// Import useTheme dari context ThemeContext untuk ambil tema (dark / light)
 import { useTheme } from "../../context/ThemeContext";
+
+// Import komponen FavoriteView (bagian UI) dari folder pages
 import FavoriteView from "../../pages/favorite/FavoriteView";
 
+// Membuat komponen utama FavoriteComponent
 const FavoriteComponent = () => {
+  // Ambil data "favorites" dari Redux store.
+  // Jika state.favorite.favorites belum ada, maka gunakan array kosong sebagai default.
   const favorites = useSelector((state) => state.favorite.favorites || []);
+
+  // Ambil data "theme" dari context untuk tahu tema saat ini (dark atau light).
   const { theme } = useTheme();
 
+  // Render komponen FavoriteView dengan mengirimkan props: favorites dan theme
   return <FavoriteView favorites={favorites} theme={theme} />;
 };
 
+// Export komponen supaya bisa digunakan di file lain
 export default FavoriteComponent;
